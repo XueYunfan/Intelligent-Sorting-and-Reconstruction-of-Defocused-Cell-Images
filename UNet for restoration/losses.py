bede3d33f7a2e35fff583e44cee0f0daad18d49a
Shanks <58522447+XueYunfan@users.noreply.github.com>
2022-01-17 12:15:36 +0800

    Update losses.py

--- UNet for restoration/losses.py
+++ UNet for restoration/losses.py
@@ -2,11 +2,11 @@ import tensorflow.keras.backend as K
 import tensorflow as tf
 
 def SSIM_loss(y_true, y_pred):
-	loss = 1-tf.image.ssim(y_true,y_pred,max_val=1)
+	loss = 1-tf.image.ssim(y_true,y_pred,max_val=255)
 	return loss
 	
 def SSIM(y_true, y_pred):
-	loss = tf.image.ssim(y_true,y_pred,max_val=1)
+	loss = tf.image.ssim(y_true,y_pred,max_val=255)
 	return loss
 
 def l1_loss(y_true, y_pred):
